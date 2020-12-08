7d5c957e434d3579f3b98df0794cc4d75403ebd7
Rebecca <graham.cali@gmail.com>
2020-12-08 12:17:27 -0600

    commit tree 2d91fa822841d0499437c0b24457a3e25c2b27b6

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (1,'Mr. Chocolate', 9, 'M', 'brown', 'calm', 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (2,'Rowdy', 15, 'F', 'light brown', 'rowdy', 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES  (3,'Tabitha', 33, 'F', 'black', 'mean', 0);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES  (4,'Sergeant Brown', 39, 'M', 'dark brown', 'judgemental', 0);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES  (5,'Melissa', 5, 'F', 'white', 'playful', 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES  (6,'Grinch', 2, 'F', 'grey', 'relaxed', 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES  (7,'Wendy', 19, 'F', 'red', 'anxious', 0);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (8,NULL, 12, 'M', 'black', 'aggressive', 0);
+
